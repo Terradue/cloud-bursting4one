9cf2cc5df78980fad8b53ae064f0b70fcddc5870
Cesare Rossi <cesare.rossi@terradue.com>
2015-01-12 18:12:52 +0100

    Changed ruby lib structure

---
+++ src/lib/bursting.rb
@@ -0,0 +1,22 @@
+# -------------------------------------------------------------------------- #
+# Copyright 2015, Terradue S.r.l.                                            #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+# -------------------------------------------------------------------------- #
+
+module Bursting; end
+
+require 'bursting/factory/driver_factory'
+require 'bursting/factory/jclouds_factory'
+require 'bursting/drivers/bursting_driver'
+require 'bursting/drivers/jclouds_driver'
