be4357c81309f3a84ddd5aaed08252dbef0b1d1e
Cesare Rossi <cesare.rossi@terradue.com>
2015-07-22 07:55:55 +0200

    Fixed post_install softlinks

--- src/main/resources/scripts/post_install.sh
+++ src/main/resources/scripts/post_install.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
-prefix="/var/lib/one/remotes"
-
-# Creating the soft links
-for driver in jclouds cloudstack
-do
-    # vmm
-    src="${prefix}/vmm/bursting"
-    dest="${prefix}/vmm/${driver}"
-    ln -s ${src} ${dest}
-    
-    # im
-    src="${prefix}/im/bursting.d"
-    dest="${prefix}/im/${driver}.d"
-    ln -s ${src} ${dest}
-done
+gem install jsonpath
+easy_install cloudmonkey
 
 exit 0
